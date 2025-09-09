fc767bba435f28c16f639054f318ff4e795ccfc9
Vladimir Buravlev <Vladimir.Buravlev@virtoway.com>
2025-09-09 10:55:04 +0200

    VM-1627: state machine quote request type registration

--- src/VirtoCommerce.MarketplaceQuoteModule.Web/Scripts/module.js
+++ src/VirtoCommerce.MarketplaceQuoteModule.Web/Scripts/module.js
@@ -1,5 +1,5 @@
 // Call this to register your module to main application
-var moduleName = 'VirtoCommerce.MarketplaceQuoteModule';
+var moduleName = 'virtoCommerce.marketplaceQuoteModule';
 
 if (AppDependencies !== undefined) {
     AppDependencies.push(moduleName);
@@ -8,36 +8,45 @@ if (AppDependencies !== undefined) {
 angular.module(moduleName, [])
     .config(['$stateProvider',
         function ($stateProvider) {
-            $stateProvider
-                .state('workspace.MarketplaceQuoteModuleState', {
-                    url: '/marketplace-quote-module',
-                    templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
-                    controller: [
-                        'platformWebApp.bladeNavigationService',
-                        function (bladeNavigationService) {
-                            var newBlade = {
-                                id: 'blade1',
-                                controller: 'VirtoCommerce.MarketplaceQuoteModule.helloWorldController',
-                                template: 'Modules/$(VirtoCommerce.MarketplaceQuoteModule)/Scripts/blades/hello-world.html',
-                                isClosingDisabled: true,
-                            };
-                            bladeNavigationService.showBlade(newBlade);
-                        }
-                    ]
-                });
+        //    $stateProvider
+        //        .state('workspace.MarketplaceQuoteModuleState', {
+        //            url: '/marketplace-quote-module',
+        //            templateUrl: '$(Platform)/Scripts/common/templates/home.tpl.html',
+        //            controller: [
+        //                'platformWebApp.bladeNavigationService',
+        //                function (bladeNavigationService) {
+        //                    var newBlade = {
+        //                        id: 'blade1',
+        //                        controller: 'VirtoCommerce.MarketplaceQuoteModule.helloWorldController',
+        //                        template: 'Modules/$(VirtoCommerce.MarketplaceQuoteModule)/Scripts/blades/hello-world.html',
+        //                        isClosingDisabled: true,
+        //                    };
+        //                    bladeNavigationService.showBlade(newBlade);
+        //                }
+        //            ]
+        //        });
         }
     ])
     .run(['platformWebApp.mainMenuService', '$state',
-        function (mainMenuService, $state) {
+        'virtoCommerce.stateMachineModule.stateMachineTypes',
+        function (mainMenuService, $state,
+            stateMachineTypes) {
             //Register module in main menu
-            var menuItem = {
-                path: 'browse/marketplace-quote-module',
-                icon: 'fa fa-cube',
-                title: 'MarketplaceQuoteModule',
-                priority: 100,
-                action: function () { $state.go('workspace.MarketplaceQuoteModuleState'); },
-                permission: 'marketplace-quote-module:access',
-            };
-            mainMenuService.addMenuItem(menuItem);
+            //var menuItem = {
+            //    path: 'browse/marketplace-quote-module',
+            //    icon: 'fa fa-cube',
+            //    title: 'MarketplaceQuoteModule',
+            //    priority: 100,
+            //    action: function () { $state.go('workspace.MarketplaceQuoteModuleState'); },
+            //    permission: 'marketplace-quote-module:access',
+            //};
+            //mainMenuService.addMenuItem(menuItem);
+
+            // Quote request state machine entity type registration
+            stateMachineTypes.addType({
+                caption: 'marketplaceQuote.state-machine-entity-types.quote-request',
+                value: 'VirtoCommerce.QuoteModule.Core.Models.QuoteRequest'
+            });
+
         }
     ]);
